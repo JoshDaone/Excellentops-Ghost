413545470d475ddbbd177c6ad8d5ea9c44ce52f5
Joshua Tu <tujunxiong@live.com>
2020-12-21 16:04:33 +0000

    Add v3.38.0

--- core/server/api/canary/utils/serializers/output/preview.js
+++ core/server/api/canary/utils/serializers/output/preview.js
@@ -5,6 +5,5 @@ module.exports = {
         frame.response = {
             preview: [mapper.mapPost(model, frame)]
         };
-        frame.response.preview[0].page = model.get('type') === 'page';
     }
 };
