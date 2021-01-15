c3d27d196a7ed2556a2f838ae189ee8a843620dc
Joshua Tu <tujunxiong@live.com>
2021-01-15 16:03:53 +0000

    Add v3.38.0

--- core/server/services/jobs/job-service.js
+++ core/server/services/jobs/job-service.js
@@ -5,13 +5,7 @@
 
 const JobManager = require('@tryghost/job-manager');
 const logging = require('../../../shared/logging');
-const sentry = require('../../../shared/sentry');
 
-const errorHandler = (error, workerMeta) => {
-    logging.info(`Capturing error for worker during execution of job: ${workerMeta.name}`);
-    logging.error(error);
-    sentry.captureException(error);
-};
-const jobManager = new JobManager({logging, errorHandler});
+const jobManager = new JobManager(logging);
 
 module.exports = jobManager;
