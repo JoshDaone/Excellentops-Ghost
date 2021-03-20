e28fe4e7e299486dabd254ea9f3f16744d738deb
Joshua Tu <tujunxiong@live.com>
2021-03-20 16:03:56 +0000

    Add v3.38.0

--- core/server/services/mega/post-email-serializer.js
+++ core/server/services/mega/post-email-serializer.js
@@ -156,8 +156,7 @@ const serialize = async (postModel, options = {isBrowserPreview: false}) => {
         showSiteHeader: settingsCache.get('newsletter_show_header'),
         bodyFontCategory: settingsCache.get('newsletter_body_font_category'),
         showBadge: settingsCache.get('newsletter_show_badge'),
-        footerContent: settingsCache.get('newsletter_footer_content'),
-        accentColor: settingsCache.get('accent_color')
+        footerContent: settingsCache.get('newsletter_footer_content')
     };
     let htmlTemplate = template({post, site: getSite(), templateSettings});
     if (options.isBrowserPreview) {
